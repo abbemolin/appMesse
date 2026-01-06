8abc3c95a6dae934ccfd0603d8ac9ace8d69ef37
abbemolin <croixglorieusecure@gmail.com>
2026-01-06 22:43:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
-// Configuration de l'application
-const supabaseUrl = 'https://pmeaimfcwtykhqtueomd.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtZWFpbWZjd3R5a2hxdHVlb21kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3MzA4MzIsImV4cCI6MjA4MzMwNjgzMn0.X91Usnq8WtZYuY40UFIo7Xl8P1O46LKqe1GvJ7RIWYE';
-
-// Initialisation du client Supabase
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+// On vérifie si Supabase est déjà initialisé pour éviter l'erreur "already declared"
+if (typeof supabase === 'undefined') {
+    const supabaseUrl = 'https://pmeaimfcwtykhqtueomd.supabase.co';
+    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtZWFpbWZjd3R5a2hxdHVlb21kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3MzA4MzIsImV4cCI6MjA4MzMwNjgzMn0.X91Usnq8WtZYuY40UFIo7Xl8P1O46LKqe1GvJ7RIWYE';
+    
+    // On crée la variable globale
+    var supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+}
 
 console.log("Configuration Supabase chargée.");
