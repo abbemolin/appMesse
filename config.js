ff402d97cd463112d0a9572e94321445872d422c
abbemolin <croixglorieusecure@gmail.com>
2026-01-06 22:45:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
-// On vérifie si Supabase est déjà initialisé pour éviter l'erreur "already declared"
-if (typeof supabase === 'undefined') {
-    const supabaseUrl = 'https://pmeaimfcwtykhqtueomd.supabase.co';
-    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtZWFpbWZjd3R5a2hxdHVlb21kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3MzA4MzIsImV4cCI6MjA4MzMwNjgzMn0.X91Usnq8WtZYuY40UFIo7Xl8P1O46LKqe1GvJ7RIWYE';
-    
-    // On crée la variable globale
-    var supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
-}
+// On définit les clés
+const _supabaseUrl = 'https://pmeaimfcwtykhqtueomd.supabase.co';
+const _supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtZWFpbWZjd3R5a2hxdHVlb21kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3MzA4MzIsImV4cCI6MjA4MzMwNjgzMn0.X91Usnq8WtZYuY40UFIo7Xl8P1O46LKqe1GvJ7RIWYE';
 
-console.log("Configuration Supabase chargée.");
+// On crée le client dans une variable globale nommée "maConnexion"
+// Cela évite de se mélanger avec le mot "supabase" qui est réservé à la bibliothèque
+var maConnexion = supabase.createClient(_supabaseUrl, _supabaseKey);
+
+console.log("Client Supabase initialisé dans 'maConnexion'");
