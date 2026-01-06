9af356464d41b3eaef6a129c0d30d8ed2b504edf
abbemolin <croixglorieusecure@gmail.com>
2026-01-06 22:23:47 +0100

    Delete AppMesse directory

--- AppMesse/config.js
+++
@@ -1,8 +0,0 @@
-// Configuration de l'application
-const supabaseUrl = 'https://pmeaimfcwtykhqtueomd.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtZWFpbWZjd3R5a2hxdHVlb21kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3MzA4MzIsImV4cCI6MjA4MzMwNjgzMn0.X91Usnq8WtZYuY40UFIo7Xl8P1O46LKqe1GvJ7RIWYE';
-
-// Initialisation du client Supabase
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
-
-console.log("Configuration Supabase chargée.");
