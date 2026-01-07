17701035684582fa192dab7f72be609dd280d5a5
abbemolin <croixglorieusecure@gmail.com>
2026-01-07 23:12:53 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,43 @@
+const CACHE_NAME = 'messes-v1';
+
+// Liste des fichiers à mettre en cache pour le mode hors-ligne
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './intentions.html',
+    './planification.html',
+    './style.css',
+    './config.js',
+    './menu.js',
+    './manifest.json'
+];
+
+// Installation : on met les fichiers en cache
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+// Activation : on nettoie les anciens caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Fetch : on répond avec le cache si disponible, sinon on va sur le réseau
 self.addEventListener('fetch', (event) => {
-    // Permet l'installation PWA sur Android
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
